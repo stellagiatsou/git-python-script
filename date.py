41be841edccf041f8b35d437a8ab479cf9b26bb9
Stella Giatsou <113286475+stellagiatsou@users.noreply.github.com>
2025-02-16 16:02:53 +0200

    Update date.py

--- date.py
+++ date.py
@@ -2,3 +2,9 @@ from datetime import date
 
 today = date.today()
 print("Current date: ", today)
+
+count = 0  # Initialize the variable
+
+for i in range(319):
+    count += 1  # Increase count
+    print(f"Current count: {count}")
